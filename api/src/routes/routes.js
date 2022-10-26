a49478be17e42a74a0591dd4f1501cc268ea1ba1
conormondo <conor.manning@mondoshop.com>
2022-10-26 07:42:06 -0500

    Random Sandwich. Basic API

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,29 +1,24 @@
 const { json } = require('body-parser');
 const express = require('express');
-const fs = require('fs');
 const { randomView, sandwichOverview, tableView} = require('../controllers/sandwichController');
-
-const filepath = '/Users/conor.manning/projects/portfolio-site/api/src/recs.json';
+const { apiServe } = require('../controllers/apiController');
 
 const router = express.Router();
 
+// Base Navigation
 router.get('/', (req, res) => {
     res.render('index')
 });
 router.get('/about', (req, res) => {
     res.render('about')
 });
-router.get('/sandwiches', sandwichOverview);
 
+// Sandwich Section
+router.get('/sandwiches', sandwichOverview);
 router.get('/sandwiches/random', randomView);
 router.get('/sandwiches/all', tableView);
 
-router.get('/data', (req, res) => {
-    fs.readFile(filepath, 'utf8', (err,data) => {
-        if (err) {
-            throw err;
-        }
-        res.send(JSON.parse(data));
-    });
-});
+// API Endpoint
+router.get('/data', apiServe);
+
 module.exports = router
