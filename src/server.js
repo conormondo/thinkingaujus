0b8d7c58e3cb3686bdb1a58e63383275f7324811
conormondo <conor.manning@mondoshop.com>
2023-04-30 23:27:59 -0500

    Switch to NPM packages

--- src/server.js
+++ src/server.js
@@ -6,11 +6,15 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 var favicon = require('serve-favicon');
 
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap-table/dist')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap-table/dist')));
 app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
