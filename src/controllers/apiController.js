b1231423202e0c2663697f677a4c83b370aa99dc
Conor Manning <87834064+conormondo@users.noreply.github.com>
2023-09-30 14:38:26 -0500

    Sandwich page (#11)
    Initial sandwich page layout before DB initialization.

---
+++ src/controllers/apiController.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filepath = '/Users/conor.manning/projects/thinkingaujus/src/MOCK_DATA.json';
+
+const apiServe = (req, res) => {
+    const filters = req.query;
+    // console.log(filters);
+
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        let jsonData = JSON.parse(data);
+        if (filters.recommended) {
+            mask = filters.recommended;
+            var isTrueSet = (mask.toLowerCase() === 'true');
+            let mapped = jsonData.filter(
+                elem => elem.recommended == isTrueSet
+            )
+            res.send(mapped);
+        } else {
+            res.send(jsonData);
+        }
+    });
+};
+
+module.exports = {
+    apiServe
+};
--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,4 @@
+
 // Grabs age date
 function getAge() {
     const bd = new Date(1994, 03, 12);
@@ -9,3 +10,49 @@ function getAge() {
 function updateAge() {
     document.body.querySelector('#age').innerText = getAge();
 }
+
+function showSandwich(recsJson) {
+    const recName = document.getElementById('recName');
+    const recBlurb = document.getElementById('recBlurb');
+    const recId = document.getElementById('recId');
+    const recRestaurant = document.getElementById('recRestaurant');
+
+    let choice = Math.floor(Math.random() * recsJson.length - 1);
+    let sandwich = recsJson[choice]
+
+    recName.innerText = sandwich.name;
+    recBlurb.innerText = sandwich.blurb;
+    recId.innerText = sandwich.id;
+    recRestaurant.innerText = sandwich.restaurant;
+
+    // console.log('Randomly selected number: ', choice + 1)
+    // console.log(recsJson[choice])
+}
+
+// Random sandwich call. With only recommended or not check.
+
+function getRandomSandwich() {
+    var endpoint = '/data.json';
+
+    let recSwitch = document.getElementById('recommendSwitch');
+    if (recSwitch.checked) {
+        var _query = '?recommended=' + true;
+        var endpoint = endpoint + _query;
+    }
+    // TODO: Make work
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => showSandwich(data));
+// Grabs age date
+function getAge() {
+    const bd = new Date(1994, 03, 12);
+    let elapsed = Date.now() - bd
+    let age = elapsed / 1000 / 60 / 60 / 24 / 365
+    return Math.floor(age)
+}
+// Updates age element
+function updateAge() {
+    document.body.querySelector('#age').innerText = getAge();
+}
+    return;
+};
